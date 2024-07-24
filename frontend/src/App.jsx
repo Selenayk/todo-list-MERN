8edb6cb3109b478f4e215017693122e60c12583e
Selenayk <selenaykeskinm@gmail.com>
2024-07-24 15:44:54 +0300

    Added Log In Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LogInPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/auth/register" element={<RegisterPage />} />
+      <Route path='/auth/login' element={<LoginPage />}/>
     </Routes>
   );
 };
--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -1,46 +1,34 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { register } from '../api/api';
+import axios from 'axios';
 
-const RegisterPage = () => {
-  const [username, setUsername] = useState('');
+const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [setError] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await register(username, email, password);
-      if (res.status === 201) {
+      const res = await axios.post('http://localhost:3000/api/auth/login', {
+        email,
+        password,
+      });
+
+      if (res.status === 200) {
         localStorage.setItem('token', res.data.accessToken);
-        navigate('/auth/login');
+        navigate('/'); 
       }
     } catch (error) {
-      setError(
-        'Registration failed:' + (error.res?.data?.message || error.message)
-      );
-    } 
+      console.error('Login failed:', error);
+    }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-200">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-3xl font-semibold mb-4">Sign Up</h2>
+        <h2 className="text-3xl font-semibold mb-4">Login</h2>
         <form onSubmit={handleSubmit}>
-          <div className="my-5">
-            <label className="text-l mr-4 block text-gray-700 font-semibold">
-              Username
-            </label>
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full px-3 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-cyan-500"
-            />
-          </div>
           <div className="my-5">
             <label className="text-l mr-4 block text-gray-700 font-semibold">Email</label>
             <input
@@ -65,17 +53,18 @@ const RegisterPage = () => {
             type="submit"
             className="w-full bg-cyan-500 text-white py-2 rounded-lg hover:bg-cyan-600"
           >
-            Sign Up
+            Login
           </button>
         </form>
         <p className="mt-4">
-          Already have an account?{' '}
-          <Link to="/auth/login" className="text-cyan-500">
-            Login
+          Do not have an account?{' '}
+          <Link to="/auth/register" className="text-cyan-500">
+            Sign Up
           </Link>
         </p>
       </div>
     </div>
   );
 };
-export default RegisterPage;
+
+export default LoginPage;
